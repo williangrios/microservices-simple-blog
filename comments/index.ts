1911e2570578abb0a845edf9a18d7c2d2316ad93
Willian Rios <williangrios@yahoo.com.br>
2024-05-06 16:11:09 -0300

    refactor: refactor code

--- comments/index.ts
+++ comments/index.ts
@@ -1,9 +1,11 @@
 import bodyParser from 'body-parser'
 import express, { Request, Response } from 'express'
 import { randomBytes } from 'crypto'
+import cors from 'cors'
 
 const app = express()
 app.use(bodyParser.json())
+app.use(cors())
 
 interface CommentProps {
   commentId: string
@@ -11,11 +13,32 @@ interface CommentProps {
   content: string
 }
 
-const comments: CommentProps[] = []
+const comments: CommentProps[] = [
+  {
+    postId: '923789d23h',
+    commentId: '923789d233',
+    content: 'comentario do post 1',
+  },
+  {
+    postId: '923729d23h',
+    commentId: '923789d23x',
+    content: 'comentario do post 2',
+  },
+  {
+    postId: '923749d23h',
+    commentId: '923789d23a',
+    content: 'comentario do post 3',
+  },
+  {
+    postId: '923749d23h',
+    commentId: '923789d23u',
+    content: 'comentario do post 3',
+  },
+]
 
 app.get('/posts/:id/comments', (req: Request, res: Response) => {
   const postId = req.params.id
-  const commentsById = comments.find((comment) => comment.postId === postId)
+  const commentsById = comments.filter((comment) => comment.postId === postId)
   res.send(commentsById)
 })
 
@@ -32,6 +55,6 @@ app.post('/posts/:id/comments', (req: Request, res: Response) => {
   res.status(201).send(newComment)
 })
 
-app.listen(5000, () => {
-  console.log('Comments service Listen on port 5000')
+app.listen(4001, () => {
+  console.log('Comments service Listen on port 4001')
 })
--- posts/index.ts
+++ posts/index.ts
@@ -1,18 +1,33 @@
 import express, { Request, Response } from 'express'
 import bodyParser from 'body-parser'
 import { randomBytes } from 'crypto'
+import cors from 'cors'
 
 const app = express()
 app.use(bodyParser.json())
+app.use(cors())
 
 interface PostProps {
   postId: string
   title: string
 }
 
-const posts: PostProps[] = []
+const posts: PostProps[] = [
+  {
+    postId: '923789d23h',
+    title: 'post 1',
+  },
+  {
+    postId: '923729d23h',
+    title: 'post 2',
+  },
+  {
+    postId: '923749d23h',
+    title: 'post 3',
+  },
+]
 
-app.get('/posts', (res: Response) => {
+app.get('/posts', (req: Request, res: Response) => {
   res.send(posts)
 })
 
